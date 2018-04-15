7b589fb46f73612b65b96fb51e934670fc5d0345
ylyiue <yalan.yiue@epfl.ch>
2018-04-15 22:11:46 +0200

    Json voila! (with clean csv)

---
+++ OCR/getJson.py
@@ -0,0 +1,20 @@
+# coding=UTF-8
+
+import os
+import requests
+
+for root, dirs, files in os.walk('LandNumber/'):
+    for file in files:
+        if file.endswith(".csv"):
+
+            url = 'http://twland.ronny.tw/index/search?'
+
+            csv_file = open('LandNumber/' + file, 'r')
+            for line in csv_file.readlines():
+                url += 'lands[]=臺北市,' + line.strip() + '&'
+            # print(url)
+            json_str = requests.get(url).text
+            # print('json',json_str)
+            json_file = os.path.splitext(file)[0] + '.json'
+            with open('GeoJson/' + json_file, 'w') as out_file:
+                out_file.write(json_str)
