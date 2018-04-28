f978743a60d91dcc0e7fcb4c6c927328a62b8874
GrimReaperSam <fayezlahoud@gmail.com>
2018-04-28 18:43:44 +0200

    Adding faulty pdfs

--- OCR/getJson.py
+++ OCR/getJson.py
@@ -4,20 +4,22 @@ import os
 import requests
 
 for root, dirs, files in os.walk('LandNumber/'):
-    for file in files:
-        if file.endswith("0657.csv"):
+	for file in files:
+		json_file = os.path.splitext(file)[0] + '.json'
+		json_file_path = 'GeoJson/' + json_file
+		if os.path.exists(json_file_path):
+			continue
 
-            url = 'http://twland.ronny.tw/index/search?'
+		url = 'http://twland.ronny.tw/index/search?'
 
-            csv_file = open('LandNumber/' + file, 'r', encoding='utf-8')
-            print("[Dealing] " + file)
-            for line in csv_file.readlines():
-                url += 'lands[]=臺北市,' + line.strip() + '&'
-            # print(url)
-            json_str = requests.get(url).text
-            if json_str == '{\"type\":\"FeatureCollection\",\"features\":[]}':
-                print(' --- EMPTY --- ' + file)
-            # print('json',json_str)
-            json_file = os.path.splitext(file)[0] + '.json'
-            with open('GeoJson/' + json_file, 'w') as out_file:
-                out_file.write(json_str)
+		csv_file = open('LandNumber/' + file, 'r', encoding='utf-8')
+		print("[Dealing] " + file)
+		for line in csv_file.readlines():
+			url += 'lands[]=臺北市,' + line.strip() + '&'
+		# print(url)
+		json_str = requests.get(url).text
+		if json_str == '{\"type\":\"FeatureCollection\",\"features\":[]}':
+			print(' --- EMPTY --- ' + file)
+		# print('json',json_str)
+		with open(json_file_path, 'w') as out_file:
+			out_file.write(json_str)
--- Scripts/AddAll.py
+++ Scripts/AddAll.py
@@ -1,15 +1,24 @@
 import glob
 import re
 from pathlib import Path
+import pandas as pd
 from Scripts.Utils import *
 
-files = glob.glob('%s/OCR/GeoJson/*.json' % base_dir)
-for f in files:
-	p = Path(f)
+# files = glob.glob('%s/OCR/GeoJson/*.json' % base_dir)
+
+Renewals_dataset = pd.read_csv('%sRenewals.csv' % data_dir)
+rooftops = Renewals_dataset['rooftopsNearby'].as_matrix()
+names = Renewals_dataset['district'].as_matrix()
+for item in zip(names, rooftops):
+	current_file = '%s/OCR/GeoJson/%s.json' % (base_dir,  item[0])
+	rooftops_nearby = item[1]
+		
+	p = Path(current_file)
 	layerName = p.stem
 	vLayer = QgsVectorLayer(str(p), layerName, "ogr")
 	if vLayer.featureCount() != 0:
 		symbols = vLayer.renderer().symbol()
-		symbols.setColor(QColor.fromRgb(50, 50, 250))
+		symbols.setColor(QColor.fromRgb(128 + int(127 * rooftops_nearby / 8.0), 0, 0, 255))
+		symbols.symbolLayer(0).setStrokeColor(QColor.fromRgb(0,0,0,0))
 		QgsProject.instance().addMapLayer(vLayer)
 print("Finished")
