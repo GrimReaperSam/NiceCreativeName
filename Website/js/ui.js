e87cd422c87ba61a77d801149824864217515622
ylyiue <yalan.yiue@epfl.ch>
2018-05-30 00:27:53 +0200

    Finished?

--- Website/js/ui.js
+++ Website/js/ui.js
@@ -51,8 +51,7 @@ require([
 ) {
   /*
     Initialize ArcGIS Maps Views
-*/
-
+  */
   var initExt = new esri.geometry.Extent({
     xmin: 13520000,
     ymin: 2871000,
@@ -105,8 +104,7 @@ require([
 
   /*
     Bind right map with the left extent
-*/
-
+  */
   var leftConnection = dojo.connect(lMap, "onExtentChange", leftChangeHandler);
 
   function leftChangeHandler(ext) {
@@ -115,8 +113,7 @@ require([
 
   /*
     Load historic maps
-*/
-
+  */
   years.forEach(function(year) {
     mapServer =
       "https://www.historygis.udd.gov.taipei/arcgis/rest/services/Aerial/Ortho_" +
@@ -137,9 +134,8 @@ require([
   changeYear(2012, 2017);
 
   /*
-    Year selection slider
-*/
-
+    Years selection slider
+  */
   $("#year_slider").ionRangeSlider({
     type: "double",
     grid: true,
@@ -156,11 +152,13 @@ require([
   });
 
   function changeYear(from, to) {
+    dehighlight();
     if (from != lYear) {
       lYearLayers[lYear].hide();
       lYearLayers[from].show();
       lYear = from;
       displayGeoJsonLayer(true);
+      clearStat();
     }
     if (to != rYear) {
       rYearLayers[rYear].hide();
@@ -173,7 +171,6 @@ require([
   /*
     Load rooftop csv
   */
-
   var dotMarker = new SimpleMarkerSymbol(
     "solid",
     13,
@@ -196,7 +193,6 @@ require([
     Load Final.csv using papaparse.js
     Save as {rid:{attr:val}} dictionary
   */
-
   Papa.parse("data/Final.csv", {
     delimiter: ",",
     header: true,
@@ -212,9 +208,8 @@ require([
   });
 
   /*
-    Load GeoJson file
+    Load GeoJson file (renewal areas)
   */
-
   var polyMarker = new SimpleFillSymbol(
     "solid",
     null,
@@ -227,8 +222,9 @@ require([
     null,
     new Color([241, 195, 5, 0.6])
   );
-  var polyHighlightRenderer = new SimpleRenderer(highlightMarker);
+
   var highlight;
+  var hasHighlight = false;
 
   lMap.on("load", function() {
     for (var rid in data) {
@@ -256,19 +252,21 @@ require([
     //   geoJsonLayer.maxScale = 0;
     // });
 
+    /*
+      When a renew area on the LEFT map is selected:
+        1. Show statistics of both years
+        2. Highlight selected renew area in the right map
+      When a renew area on the RIGHT map is selected:
+        1. Show statistics of right year
+    */
     geoJsonLayer.on("click", function(c) {
       if (isLeft) {
-        highlight = new GraphicsLayer();
-        rRenewLayers[rid].graphics.forEach(function(g) {
-          var gClone = new Graphic(g.toJson());
-          gClone.setSymbol(highlightMarker);
-          highlight.add(gClone);
-        });
-        rMap.addLayer(highlight);
-      }
+        highlightRightMap(rid);
+        setStat(rid, true);
+      } else setStat(rid, false);
     });
 
-    // change cursor to indicate features are click-able
+    // Change cursor to indicate features are click-able
     geoJsonLayer.on("mouse-over", function() {
       if (isLeft) lMap.setMapCursor("pointer");
       else rMap.setMapCursor("pointer");
@@ -278,7 +276,7 @@ require([
       else rMap.setMapCursor("default");
     });
 
-    // Zoom-in to one guy
+    // Zoom-in to this guy
     geoJsonLayer.on("update-end", function(e) {
       if (rid == "松山區B0647") lMap.setExtent(e.target.extent.expand(5));
     });
@@ -291,10 +289,117 @@ require([
     return geoJsonLayer;
   }
 
-  lMap.infoWindow.on('hide', function(){
-    rMap.removeLayer(highlight);
-  })
+  // Highlight selected renew area in the right map
+  // (with ugly work-around for cloning GeoJsonLayer)
+  function highlightRightMap(rid) {
+    dehighlight();
+    highlight = new GraphicsLayer();
+    rRenewLayers[rid].graphics.forEach(function(g) {
+      var gClone = new Graphic(g.toJson());
+      gClone.setSymbol(highlightMarker);
+      highlight.add(gClone);
+    });
+    rMap.addLayer(highlight);
+    hasHighlight = true;
+  }
 
+  function setStat(rid, isLeft) {
+    if (isLeft) {
+      var pl = lYear > 2012 ? price(data[rid]["CurrentValue " + lYear]) : "-";
+      var gl = lYear > 2012 ? green(data[rid]["greeniness " + lYear]) : "-";
+      $("#l-price").text(pl);
+      $("#l-green").text(gl);
+      $("#l-roofNum").text(data[rid]["rooftopsNearby"]);
+    }
+    var pr = rYear > 2012 ? price(data[rid]["CurrentValue " + rYear]) : "-";
+    var gr = rYear > 2012 ? green(data[rid]["greeniness " + rYear]) : "-";
+    $("#r-price").text(pr);
+    $("#r-green").text(gr);
+    $("#r-roofNum").text(data[rid]["rooftopsNearby"]);
+    setStatColor();
+  }
+
+  function price(v) {
+    if (v > 1000000) return Math.round(v / 1000000);
+    else return (v / 1000000).toFixed(1);
+  }
+
+  function green(v) {
+    return Math.round(v * 100);
+  }
+
+  function setStatColor() {
+    var grey = "#7C8D94",
+      yellow = "#f18805",
+      green = "#03b5aa",
+      blue = "#2274a5";
+    //$(this).css("color", "#ffffff");
+    var lp = $("#l-price").text() == "-" ? 0 : parseFloat($("#l-price").text());
+    var rp = $("#r-price").text() == "-" ? 0 : parseFloat($("#r-price").text());
+    if (lp > rp) {
+      $("#l-price").css("color", blue);
+      $("#r-price").css("color", grey);
+    } else if (lp < rp) {
+      $("#r-price").css("color", blue);
+      $("#l-price").css("color", grey);
+    } else {
+      $("#r-price").css("color", blue);
+      $("#l-price").css("color", blue);
+    }
+    var lg = $("#l-green").text() == "-" ? 0 : parseInt($("#l-green").text());
+    var rg = $("#r-green").text() == "-" ? 0 : parseInt($("#r-green").text());
+    if (lg > rg) {
+      $("#l-green").css("color", green);
+      $("#r-green").css("color", grey);
+    }
+    else if (lg < rg) {
+      $("#r-green").css("color", green);
+      $("#l-green").css("color", grey);
+    } 
+    else {
+      $("#r-green").css("color", green);
+      $("#l-green").css("color", green);
+    }
+    var lr = parseInt($("#l-roofNum").text());
+    var rr = parseInt($("#r-roofNum").text());
+    if (lr > rr) {
+      $("#l-roofNum").css("color", yellow);
+      $("#r-roofNum").css("color", grey);
+    } if (lr < rr) {
+      $("#r-roofNum").css("color", yellow);
+      $("#l-roofNum").css("color", grey);
+    } else {
+      $("#r-roofNum").css("color", yellow);
+      $("#l-roofNum").css("color", yellow);
+    }
+  }
+
+  function clearStat() {
+    $("#l-price").text("");
+    $("#l-green").text("");
+    $("#l-roofNum").text("");
+    $("#r-price").text("");
+    $("#r-green").text("");
+    $("#r-roofNum").text("");
+  }
+
+  /*
+    De-highlight from right map when Pop-up is closed
+  */
+  lMap.infoWindow.on("hide", function() {
+    dehighlight();
+  });
+
+  function dehighlight() {
+    if (hasHighlight) {
+      rMap.removeLayer(highlight);
+      hasHighlight = false;
+    }
+  }
+
+  /*
+    Show renewal areas according to years
+  */
   function displayGeoJsonLayer(isLeft) {
     for (var rid in data) {
       if (isLeft) {
@@ -311,7 +416,7 @@ require([
 
   /*
     Checkboxes for hiding/showing renew areas & rooftops
-*/
+  */
 
   $("#l-renew").iCheck({
     checkboxClass: "icheckbox_flat-grey",
@@ -326,6 +431,7 @@ require([
   $("#l-renew").on("ifUnchecked", function(event) {
     lRenewShown = false;
     displayGeoJsonLayer(true);
+    dehighlight();
   });
 
   $("#r-renew").iCheck({
@@ -341,6 +447,7 @@ require([
   $("#r-renew").on("ifUnchecked", function(event) {
     rRenewShown = false;
     displayGeoJsonLayer(false);
+    dehighlight();
   });
 
   $("#l-roof").iCheck({
